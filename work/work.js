101940340bb9c74ef9673fcd814cbc35caf2b556
Lindsay <lauder@usc.edu>
2017-12-08 00:27:38 -0800

    edits

---
+++ work/work.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    
+    $(".d").css('background-color', 'transparent').css('color', '#000000').css('padding-left', '0px').css('padding-right', '0px');
+    
+});
+
+
+//colors
+
+
+
+$("#design1").mouseover(function() {
+    
+    $("#design1").css('background-color', '#f2634b').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#design2").mouseover(function() {
+    
+    $("#design2").css('background-color', '#f2634b').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#design3").mouseover(function() {
+    
+    $("#design3").css('background-color', '#f2634b').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#design4").mouseover(function() {
+    
+    $("#design4").css('background-color', '#f2634b').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#photo1").mouseover(function() {
+    
+    $("#photo1").css('background-color', '#6a7f23').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#photo2").mouseover(function() {
+    
+    $("#photo2").css('background-color', '#6a7f23').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#photo3").mouseenter(function() {
+    
+    $("#photo3").css('background-color', '#6a7f23').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#other1").mouseover(function() {
+    
+    $("#other1").css('background-color', '#f6cac9').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#other2").mouseover(function() {
+    
+    $("#other2").css('background-color', '#f6cac9').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#other3").mouseover(function() {
+    
+    $("#other3").css('background-color', '#f6cac9').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$("#other4").mouseover(function() {
+    
+    $("#other4").css('background-color', '#f6cac9').css('color', '#ffffff').css('padding-left', '20px').css("padding-right", '20px');
+});
+
+$(".d").mouseout(function() {
+    
+    $(".d").css('background-color', 'transparent').css('color', '#000000').css('padding-left', '0px').css('padding-right', '0px');
+    
+});
+
